d3a29bbdfafae7f4a6aab799b0470b62116facf4
dMario24 <data.mario24@gmail.com>
2024-08-23 17:38:46 +0900

    ptc-sw old ver

--- src/pytxtchat/cli.py
+++ src/pytxtchat/cli.py
@@ -1,5 +1,11 @@
 from pytxtchat.com import pong
+from pytxtchat.textual import StopwatchApp
 
 def ping():
     msg = pong()
     print(msg)
+
+
+def run_stopwatch():
+    app = StopwatchApp()
+    app.run()
