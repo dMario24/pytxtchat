ff3655ab24c88f24d46926fb91cbf735a125497f
dMario24 <data.mario24@gmail.com>
2024-08-26 09:12:08 +0900

    Basic Chat Form
    - https://velog.io/@earthquake_woo/Python-Textual-%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%9F%AC%EB%A6%AC%EB%A1%9C-TUI-App-%EA%B0%9C%EB%B0%9C%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-%ED%99%9C%EC%9A%A9

---
+++ src/pytxtchat/textual/chat.py
@@ -0,0 +1,52 @@
+from textual import on
+from textual.app import App, ComposeResult
+from textual.screen import Screen
+from textual.containers import Container, Vertical
+from textual.widgets import Input, RichLog, Button
+
+
+class HomeScreen(Screen):
+    
+    def compose(self) -> ComposeResult:
+        yield Container(
+            Button("Get Started", id="get_started", classes="button_widget"),
+            Button("Exit", id="exit", classes="button_widget"),
+            classes="home_screen_container",
+        )
+        
+    @on(Button.Pressed)
+    def button_pressed_handler(self, event: Button.Pressed) -> None:
+        if event.button.id == "get_started":
+            self.app.push_screen(ChatScreen())
+        elif event.button.id == "exit":
+            self.app.exit()
+            
+            
+class ChatScreen(Screen):
+    
+    def compose(self) -> ComposeResult:
+        yield Vertical(
+            RichLog(classes="richlog_widget"),
+            Input(placeholder="Enter chat", classes="input_widget"),
+            classes="vertical_layout"
+        )
+                
+    @on(Input.Submitted)
+    def input_submitted_handler(self, event: Input.Submitted):
+        log = self.query_one(RichLog)
+        log.write(f"me: {event.value}")
+        input = self.query_one(Input)
+        input.value = ""    
+    
+
+
+class ChatApp(App):
+    CSS_PATH = "chat.css"
+    
+    def on_mount(self) -> None:
+        self.app.push_screen(HomeScreen())
+
+
+if __name__  == "__main__":
+    app=ChatApp()
+    app.run()
