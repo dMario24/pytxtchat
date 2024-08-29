dd2af38b03850a194fc5a91fbc7e75a71ac04695
dMario24 <data.mario24@gmail.com>
2024-08-29 09:09:15 +0900

    @bot "노스맨"

--- src/pytxtchat/bot/movie_bot.py
+++ src/pytxtchat/bot/movie_bot.py
@@ -1,5 +1,6 @@
 from pytxtchat.kafka.consumer import create_consumer
 from pytxtchat.kafka.producer import create_producer
+from pytxtchat.data.manager import search_movie
 
 def run(topic_name: str, bootstrap_server:str, offset_reset='latest'):
   bootstrap_server_list = [bootstrap_server]
@@ -12,9 +13,20 @@ def run(topic_name: str, bootstrap_server:str, offset_reset='latest'):
     msg = data['msg']
 
     if "@bot" in msg:
+
+      try:
+
+        start_index = msg.index('"') + 1
+        end_index = msg.rindex('"')
+        movie_title = msg[start_index:end_index]
+        
+        r = search_movie(movie_title)
+      except:
+        r = "몰라"
+      
       bot_msg = {
             "to" : "bot",
-            "msg" : "몰라"
+            "msg" : r
       }
 
       pro.send(topic=topic_name, value=bot_msg)
